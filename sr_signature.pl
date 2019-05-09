e19f7c9ab5b826e24a4b28b2f1a93cc8bc82ce6c
Stanislaw Romanski <s.romanski@datos.pl>
2019-05-09 13:05:43 +0200

    My name

---
+++ sr_signature.pl
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------
+not log exp srand xor s qq qx xor s x x length uc ord
+and print chr ord for qw q sub tied alarm not integer
+scalar long accept write qx xor eval xor print qq q q
+xor int eval lc q m x and print chr ord for qw y rand
+open y m xor scalar srand print qq qq xor int eval lc
+qq y sqrt cos and print chr ord for qw x main and not
+strict x y or eval q s undef or hex xor utime xor ref
+print chr int ord lc foreach qw y kill int y s gt oct
